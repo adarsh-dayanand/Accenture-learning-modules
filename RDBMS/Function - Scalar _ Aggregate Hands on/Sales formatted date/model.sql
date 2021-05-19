79e29951fae6cb9a20075417e5c81c2810bdf6d4
Adarsh D <58322802+adarsh-dayanand@users.noreply.github.com>
2021-05-19 15:08:44 +0530

    Update model.sql

--- RDBMS/Function - Scalar _ Aggregate Hands on/Sales formatted date/model.sql
+++ RDBMS/Function - Scalar _ Aggregate Hands on/Sales formatted date/model.sql
@@ -1,5 +1,2 @@
---select salesid,sales_date as FormattedDate from sales_info
---order by salesid asc;
-
-Select salesid,to_char(sales_date,'yyyy-mm-dd')as FormattedDated from sales_info 
+Select salesid,to_char(sales_date,'yyyy-mm-dd')as FORMATTED_DATE from sales_info 
 order by salesid asc; 
